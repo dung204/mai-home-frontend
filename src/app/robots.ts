f5201c289cc6cacb5dd8fbbdf2beb749d77d001f
dung204 <acezombiev4@gmail.com>
2025-07-07 14:38:45 +0700

    fix: update sitemap URL to include 'www' for correct indexing

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/user/',
     },
-    sitemap: 'https://maihome.info.vn/sitemap.xml',
+    sitemap: 'https://www.maihome.info.vn/sitemap.xml',
   };
 }
