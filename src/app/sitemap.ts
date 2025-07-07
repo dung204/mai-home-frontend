496bfbb4d8c8581e1b32b946f368261181509dd2
dung204 <acezombiev4@gmail.com>
2025-07-07 15:56:30 +0700

    fix: domain in sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -18,13 +18,13 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 
   return [
     {
-      url: 'https://www.maihome.info.com',
+      url: 'https://www.maihome.info.vn',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://www.maihome.info.com/properties',
+      url: 'https://www.maihome.info.vn/properties',
       lastModified: new Date(),
       changeFrequency: 'always',
       priority: 0.9,
@@ -32,7 +32,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ...propertyIds.map(
       (id) =>
         ({
-          url: `https://www.maihome.info.com/property/${id}`,
+          url: `https://www.maihome.info.vn/property/${id}`,
           lastModified: new Date(),
           changeFrequency: 'monthly',
           priority: 0.9,
@@ -41,14 +41,14 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ...Object.values(PropertyCategory).map(
       (category) =>
         ({
-          url: `https://www.maihome.info.com/properties?category=${category}`,
+          url: `https://www.maihome.info.vn/properties?category=${category}`,
           lastModified: new Date(),
           changeFrequency: 'always',
           priority: 0.8,
         }) satisfies MetadataRoute.Sitemap[number],
     ),
     {
-      url: 'https://www.maihome.info.com/news/all',
+      url: 'https://www.maihome.info.vn/news/all',
       lastModified: new Date(),
       changeFrequency: 'daily',
       priority: 0.6,
@@ -56,7 +56,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     ...posts.map(
       (post) =>
         ({
-          url: `https://www.maihome.info.com/news/${post}`,
+          url: `https://www.maihome.info.vn/news/${post}`,
           lastModified: new Date(),
           changeFrequency: 'never',
           priority: 0.6,
